e6e331a48d0dee5d02c0501d25bfb26552b501b8
khaiqlgd <khai.qlgd@gmail.com>
2026-09-24 16:30:41 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,542 @@
 const danhSachDiem = [
   {
-    hoTen: "Nguyễn Thị Thu Tâm",
-    sbd: "HS2602094",
-    ngaySinh: "01/01/1998",
-    diemNoi: "6.5",
-    diemNghe: "7.0",
-    diemDocViet: "6.5",
-    tongDiem: "6.7",
-    ketQua: "Đạt"
+    "hoTen": "Nguyễn Thế Anh",
+    "sbd": "HS2602095",
+    "ngaySinh": "07/05/2004",
+    "diemNoi": "6",
+    "diemNghe": "8",
+    "diemDocViet": "8.4",
+    "tongDiem": "7.47",
+    "ketQua": "Đạt"
   },
   {
-    hoTen: "Nguyễn Thế Anh",
-    sbd: "HS2602095",
-    ngaySinh: "15/05/1995",
-    diemNoi: "7.0",
-    diemNghe: "8.0",
-    diemDocViet: "7.5",
-    tongDiem: "7.5",
-    ketQua: "Đạt"
+    "hoTen": "Trần Ngọc Bình",
+    "sbd": "HS2602012",
+    "ngaySinh": "25/10/1979",
+    "diemNoi": "5",
+    "diemNghe": "7",
+    "diemDocViet": "8",
+    "tongDiem": "6.67",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Việt Chung",
+    "sbd": "HS2602019",
+    "ngaySinh": "22/05/1980",
+    "diemNoi": "7",
+    "diemNghe": "6.5",
+    "diemDocViet": "8.5",
+    "tongDiem": "7.33",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Phạm Thành Đô",
+    "sbd": "HS2602074",
+    "ngaySinh": "12/05/1988",
+    "diemNoi": "5",
+    "diemNghe": "9",
+    "diemDocViet": "6.6",
+    "tongDiem": "6.87",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Thị Thùy Dung",
+    "sbd": "HS2602020",
+    "ngaySinh": "29/10/1996",
+    "diemNoi": "6",
+    "diemNghe": "7.5",
+    "diemDocViet": "8.1",
+    "tongDiem": "7.2",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Lâm Dũng",
+    "sbd": "HS2602049",
+    "ngaySinh": "22/06/1999",
+    "diemNoi": "8.5",
+    "diemNghe": "8.5",
+    "diemDocViet": "6.6",
+    "tongDiem": "7.87",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Vũ Thị Dương",
+    "sbd": "HS2602039",
+    "ngaySinh": "26/03/1983",
+    "diemNoi": "5",
+    "diemNghe": "6",
+    "diemDocViet": "5.9",
+    "tongDiem": "5.63",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Huỳnh Quốc Gia",
+    "sbd": "HS2602002",
+    "ngaySinh": "25/01/1978",
+    "diemNoi": "5.5",
+    "diemNghe": "5.5",
+    "diemDocViet": "6.4",
+    "tongDiem": "5.8",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Thị Giản",
+    "sbd": "HS2602037",
+    "ngaySinh": "10/02/1993",
+    "diemNoi": "8.5",
+    "diemNghe": "7",
+    "diemDocViet": "6.5",
+    "tongDiem": "7.33",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Bá Giáp",
+    "sbd": "HS2602046",
+    "ngaySinh": "08/05/1989",
+    "diemNoi": "5.8",
+    "diemNghe": "8",
+    "diemDocViet": "8.4",
+    "tongDiem": "7.4",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trương Việt Hoa",
+    "sbd": "HS2602079",
+    "ngaySinh": "21/04/1974",
+    "diemNoi": "5",
+    "diemNghe": "6.5",
+    "diemDocViet": "6.4",
+    "tongDiem": "5.97",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Thế Hùng",
+    "sbd": "HS2602087",
+    "ngaySinh": "16/07/1989",
+    "diemNoi": "5.5",
+    "diemNghe": "8",
+    "diemDocViet": "8.2",
+    "tongDiem": "7.23",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Hoàng Kha",
+    "sbd": "HS2602059",
+    "ngaySinh": "21/07/1986",
+    "diemNoi": "8",
+    "diemNghe": "7",
+    "diemDocViet": "6",
+    "tongDiem": "7",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Văn Khang",
+    "sbd": "HS2602042",
+    "ngaySinh": "19/05/1975",
+    "diemNoi": "5",
+    "diemNghe": "8",
+    "diemDocViet": "7.3",
+    "tongDiem": "6.77",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Quốc Kỳ",
+    "sbd": "HS2602021",
+    "ngaySinh": "27/03/1985",
+    "diemNoi": "7",
+    "diemNghe": "7.5",
+    "diemDocViet": "5.8",
+    "tongDiem": "6.77",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Chu Thị Kim Lan",
+    "sbd": "HS2602035",
+    "ngaySinh": "25/11/1982",
+    "diemNoi": "6.5",
+    "diemNghe": "7",
+    "diemDocViet": "7.9",
+    "tongDiem": "7.13",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Thị Hồng Linh",
+    "sbd": "HS2602071",
+    "ngaySinh": "16/11/1984",
+    "diemNoi": "5",
+    "diemNghe": "7.5",
+    "diemDocViet": "6.6",
+    "tongDiem": "6.37",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Đắc Lộc",
+    "sbd": "HS2602089",
+    "ngaySinh": "26/08/1995",
+    "diemNoi": "5.5",
+    "diemNghe": "8.5",
+    "diemDocViet": "8.6",
+    "tongDiem": "7.53",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lâm Quốc Lợi",
+    "sbd": "HS2602010",
+    "ngaySinh": "02/01/1992",
+    "diemNoi": "6.3",
+    "diemNghe": "8.5",
+    "diemDocViet": "9.2",
+    "tongDiem": "8",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Hồ Minh Lợi",
+    "sbd": "HS2602014",
+    "ngaySinh": "30/04/1975",
+    "diemNoi": "5",
+    "diemNghe": "7",
+    "diemDocViet": "7",
+    "tongDiem": "6.33",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Đức Long",
+    "sbd": "HS2602062",
+    "ngaySinh": "20/11/1988",
+    "diemNoi": "7",
+    "diemNghe": "9",
+    "diemDocViet": "6.8",
+    "tongDiem": "7.6",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Ngô Thị Thanh Mai",
+    "sbd": "HS2602082",
+    "ngaySinh": "15/05/1992",
+    "diemNoi": "6",
+    "diemNghe": "9.5",
+    "diemDocViet": "6.8",
+    "tongDiem": "7.43",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Huỳnh Quang Minh",
+    "sbd": "HS2602044",
+    "ngaySinh": "02/12/1986",
+    "diemNoi": "5.5",
+    "diemNghe": "8",
+    "diemDocViet": "7.6",
+    "tongDiem": "7.03",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Thu Nam",
+    "sbd": "HS2602093",
+    "ngaySinh": "06/02/1985",
+    "diemNoi": "5.5",
+    "diemNghe": "7",
+    "diemDocViet": "8.3",
+    "tongDiem": "6.93",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Trọng Nghĩa",
+    "sbd": "HS2602027",
+    "ngaySinh": "10/08/1994",
+    "diemNoi": "5",
+    "diemNghe": "9",
+    "diemDocViet": "8.1",
+    "tongDiem": "7.37",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Quang Nghĩa",
+    "sbd": "HS2602045",
+    "ngaySinh": "08/07/1999",
+    "diemNoi": "7",
+    "diemNghe": "8",
+    "diemDocViet": "6.2",
+    "tongDiem": "7.07",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Thị Ngoan",
+    "sbd": "HS2602053",
+    "ngaySinh": "26/10/1982",
+    "diemNoi": "5.5",
+    "diemNghe": "9.5",
+    "diemDocViet": "7",
+    "tongDiem": "7.33",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nghiêm Thị Hồng Nguyên",
+    "sbd": "HS2602003",
+    "ngaySinh": "04/12/1988",
+    "diemNoi": "6.5",
+    "diemNghe": "6",
+    "diemDocViet": "6.5",
+    "tongDiem": "6.33",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Phạm Quang Nhật",
+    "sbd": "HS2602092",
+    "ngaySinh": "20/11/1979",
+    "diemNoi": "5",
+    "diemNghe": "7",
+    "diemDocViet": "8.4",
+    "tongDiem": "6.8",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Thị Yến Nhi",
+    "sbd": "HS2602097",
+    "ngaySinh": "17/05/2002",
+    "diemNoi": "7",
+    "diemNghe": "9",
+    "diemDocViet": "8.2",
+    "tongDiem": "8.07",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Thị Hồng Nhu",
+    "sbd": "HS2602011",
+    "ngaySinh": "02/11/2003",
+    "diemNoi": "5",
+    "diemNghe": "7.5",
+    "diemDocViet": "7.8",
+    "tongDiem": "6.77",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Võ Lê Anh Nhựt",
+    "sbd": "HS2602075",
+    "ngaySinh": "04/07/2003",
+    "diemNoi": "6.5",
+    "diemNghe": "9.5",
+    "diemDocViet": "6.6",
+    "tongDiem": "7.53",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Hồ Thị Yến Phi",
+    "sbd": "HS2602051",
+    "ngaySinh": "29/07/1993",
+    "diemNoi": "5",
+    "diemNghe": "8",
+    "diemDocViet": "6.6",
+    "tongDiem": "6.53",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Phan Đại Phú",
+    "sbd": "HS2602017",
+    "ngaySinh": "30/11/2004",
+    "diemNoi": "5",
+    "diemNghe": "7",
+    "diemDocViet": "7.7",
+    "tongDiem": "6.57",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Anh Phú",
+    "sbd": "HS2602060",
+    "ngaySinh": "06/02/1989",
+    "diemNoi": "5",
+    "diemNghe": "6.5",
+    "diemDocViet": "6",
+    "tongDiem": "5.83",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Ngọc Mỹ Phương",
+    "sbd": "HS2602048",
+    "ngaySinh": "07/08/1998",
+    "diemNoi": "8",
+    "diemNghe": "9.5",
+    "diemDocViet": "6.8",
+    "tongDiem": "8.1",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Mai Lâm Phương",
+    "sbd": "HS2602052",
+    "ngaySinh": "25/05/1983",
+    "diemNoi": "6",
+    "diemNghe": "8",
+    "diemDocViet": "6.4",
+    "tongDiem": "6.8",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Thanh Quan",
+    "sbd": "HS2602076",
+    "ngaySinh": "20/09/1989",
+    "diemNoi": "5.5",
+    "diemNghe": "9",
+    "diemDocViet": "6.8",
+    "tongDiem": "7.1",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Dương Phú Quốc",
+    "sbd": "HS2602015",
+    "ngaySinh": "29/05/1996",
+    "diemNoi": "5.3",
+    "diemNghe": "6.5",
+    "diemDocViet": "6.7",
+    "tongDiem": "6.17",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Đặng Văn Sỹ",
+    "sbd": "HS2602072",
+    "ngaySinh": "10/08/1984",
+    "diemNoi": "7",
+    "diemNghe": "7.5",
+    "diemDocViet": "7.2",
+    "tongDiem": "7.23",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Phạm Minh Tâm",
+    "sbd": "HS2602054",
+    "ngaySinh": "25/04/1965",
+    "diemNoi": "6",
+    "diemNghe": "7",
+    "diemDocViet": "7",
+    "tongDiem": "6.67",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Thị Thu Tâm",
+    "sbd": "HS2602094",
+    "ngaySinh": "10/11/1990",
+    "diemNoi": "5.5",
+    "diemNghe": "8.5",
+    "diemDocViet": "7.6",
+    "tongDiem": "7.2",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Hoàng Thắng",
+    "sbd": "HS2602028",
+    "ngaySinh": "17/10/1980",
+    "diemNoi": "5",
+    "diemNghe": "8",
+    "diemDocViet": "8.3",
+    "tongDiem": "7.1",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Ngọc Thanh",
+    "sbd": "HS2602038",
+    "ngaySinh": "28/09/1995",
+    "diemNoi": "8.5",
+    "diemNghe": "6.5",
+    "diemDocViet": "8",
+    "tongDiem": "7.67",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Chí Thạnh",
+    "sbd": "HS2602056",
+    "ngaySinh": "20/11/1997",
+    "diemNoi": "6",
+    "diemNghe": "9.5",
+    "diemDocViet": "7.2",
+    "tongDiem": "7.57",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Ngô Thị Kim Thoa",
+    "sbd": "HS2602005",
+    "ngaySinh": "28/10/1986",
+    "diemNoi": "6.3",
+    "diemNghe": "6.5",
+    "diemDocViet": "8.3",
+    "tongDiem": "7.03",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Bùi Văn Tiếng",
+    "sbd": "HS2602007",
+    "ngaySinh": "19/05/1985",
+    "diemNoi": "5.3",
+    "diemNghe": "8",
+    "diemDocViet": "7",
+    "tongDiem": "6.77",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Nguyễn Thị Ngọc Trang",
+    "sbd": "HS2602018",
+    "ngaySinh": "31/07/1986",
+    "diemNoi": "7.5",
+    "diemNghe": "7",
+    "diemDocViet": "8",
+    "tongDiem": "7.5",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Hoàng Triều",
+    "sbd": "HS2602078",
+    "ngaySinh": "24/08/1980",
+    "diemNoi": "5.5",
+    "diemNghe": "9",
+    "diemDocViet": "6.6",
+    "tongDiem": "7.03",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Quốc Trung",
+    "sbd": "HS2602066",
+    "ngaySinh": "02/07/2001",
+    "diemNoi": "5",
+    "diemNghe": "9",
+    "diemDocViet": "6.6",
+    "tongDiem": "6.87",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Văn Tuấn",
+    "sbd": "HS2602047",
+    "ngaySinh": "12/12/1981",
+    "diemNoi": "5.5",
+    "diemNghe": "7.5",
+    "diemDocViet": "6",
+    "tongDiem": "6.33",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Văn Tuấn",
+    "sbd": "HS2602088",
+    "ngaySinh": "06/12/1986",
+    "diemNoi": "5",
+    "diemNghe": "7",
+    "diemDocViet": "8.4",
+    "tongDiem": "6.8",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Trần Quốc Vũ",
+    "sbd": "HS2602090",
+    "ngaySinh": "02/10/1982",
+    "diemNoi": "5.5",
+    "diemNghe": "5.5",
+    "diemDocViet": "7.4",
+    "tongDiem": "6.13",
+    "ketQua": "Đạt"
+  },
+  {
+    "hoTen": "Lê Đỗ Duy Vũ",
+    "sbd": "HS2602096",
+    "ngaySinh": "14/07/2002",
+    "diemNoi": "6",
+    "diemNghe": "8.5",
+    "diemDocViet": "8.3",
+    "tongDiem": "7.6",
+    "ketQua": "Đạt"
   }
-  // Bạn thêm danh sách các thí sinh khác vào đây tương tự...
 ];
